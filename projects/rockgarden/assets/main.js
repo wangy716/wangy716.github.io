ce37e40a66a8789fcc1c7f942c5d4efc3c38925d
wangy716 <wangy716@newschool.edu>
2021-11-19 18:18:38 -0500

    final rg

--- projects/rockgarden/assets/main.js
+++ projects/rockgarden/assets/main.js
@@ -1,13 +1,8 @@
 window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
 function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
 }
-
