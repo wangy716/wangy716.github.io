09946d5a59924a1a3fb82a48835c7c4f58694912
wangy716 <wangy716@newschool.edu>
2021-11-22 17:00:56 -0500

    final version

---
+++ projects/rockgarden/assets/main.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+};
+
+
+
+function fn(){
+var img=document.getElementById("img");
+img.classList.toggle("hide");
+}
+
+
